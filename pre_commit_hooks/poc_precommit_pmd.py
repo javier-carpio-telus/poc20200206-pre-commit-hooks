52133cf1483339b7602167e2e17eebb4fff1a437
Docker Env <javier.carpio@telus.com>
2020-02-13 00:39:53 +0000

    Modified

--- pre_commit_hooks/poc_precommit_pmd.py
+++ pre_commit_hooks/poc_precommit_pmd.py
@@ -1,22 +1,28 @@
 import os
 
 def main():
+    print()
     print("*****************************************")
     print("***** POC Pre-commit/PMD java hooks *****")
     print("*****************************************")
-    
+    print()
+    print("Starting PMD validations...")
+    print()
     pmd_bin_path = "/apps/infra/precommit/pmd-bin-6.22.0-SNAPSHOT/bin"
-    command = "cd /apps/infra/precommit/poc-pipeline202002 && {}/run.sh pmd -d java-web-project/src/ -R .pmd_rulset.xml".format(pmd_bin_path)
+    command = "cd /apps/infra/precommit/poc-pipeline202002 && {}/run.sh pmd -cache -d java-web-project/src/ -R .pmd_rulset.xml".format(pmd_bin_path)
     
     pmd_stream = os.system(command)
     #pmd_output = pmd_stream.read()
 
     retv = 0
     if(pmd_stream != 0):
+        print()
         print("Execution returned code {}".format(pmd_stream))
         #print(pmd_output)
         retv = 1 # Error code 1
+    print()
     print("*****************************************")
+    print()
     return retv
 
 
