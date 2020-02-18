a9883a48b133185a2cbf6ea4cd6cb5d06f56cdd9
Docker Env <javier.carpio@telus.com>
2020-02-18 01:01:29 +0000

    Added args to poc_precommit_pmd

--- pre_commit_hooks/poc_precommit_pmd.py
+++ pre_commit_hooks/poc_precommit_pmd.py
@@ -1,28 +1,42 @@
 import os
 
-def main():
+def main(argv=None):
+
+    print(argv)
+    pmd_bin_path = "/apps/infra/precommit/pmd-bin-6.22.0-SNAPSHOT/bin"
+    workdir = "/apps/infra/precommit/poc-pipeline202002"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
     print()
     print("*****************************************")
     print("***** POC Pre-commit/PMD java hooks *****")
     print("*****************************************")
-    print()
+    print("")
     print("Starting PMD validations...")
-    print()
-    pmd_bin_path = "/apps/infra/precommit/pmd-bin-6.22.0-SNAPSHOT/bin"
-    command = "cd /apps/infra/precommit/poc-pipeline202002 && {}/run.sh pmd -cache .pmd_cache -d java-web-project/src/ -R .pmd_rulset.xml".format(pmd_bin_path)
+    print("")
+
+
+
+
+    for filename in args.filenames:
+        print("Filename {}, args: {}".format(filename, args))
+
+    command = "cd {} && {}/run.sh pmd -cache .pmd_cache -d **/src/ -R .pmd_rulset.xml".format(workdir, pmd_bin_path)
     
     pmd_stream = os.system(command)
     #pmd_output = pmd_stream.read()
 
     retv = 0
     if(pmd_stream != 0):
-        print()
+        print("")
         print("Execution returned code {}".format(pmd_stream))
         #print(pmd_output)
         retv = 1 # Error code 1
-    print()
+    print("")
     print("*****************************************")
-    print()
+    print("")
     return retv
 
 
