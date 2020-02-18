08b42f13e92a39ae2c82cdbfc2b1f4166dbc4af1
Docker Env <javier.carpio@telus.com>
2020-02-18 22:32:37 +0000

    Show args

--- pre_commit_hooks/poc_precommit_pmd.py
+++ pre_commit_hooks/poc_precommit_pmd.py
@@ -7,9 +7,9 @@ def main(argv):
     print(argv)
     pmd_bin_path = "/apps/infra/precommit/pmd-bin-6.22.0-SNAPSHOT/bin"
     workdir = "/apps/infra/precommit/poc-pipeline202002"
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*')
-    args = parser.parse_args(argv)
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('filenames', nargs='*')
+    #args = parser.parse_args(argv)
 
     print("")
     print("*****************************************")
@@ -22,8 +22,8 @@ def main(argv):
 
 
 
-    for filename in args.filenames:
-        print("Filename {}, args: {}".format(filename, args))
+    #for filename in args.filenames:
+    #    print("Filename {}, args: {}".format(filename, args))
 
     command = "cd {} && {}/run.sh pmd -cache .pmd_cache -d **/src/ -R .pmd_rulset.xml".format(workdir, pmd_bin_path)
     
